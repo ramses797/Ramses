3857a1e648e38d20adc7e5932af2a3150e301d01
Ramses797 <rmorenor97@gmail.com>
2021-01-20 12:32:50 +0100

    INICIO: controler, modelo -> creados // archivos routes renombrados

---
+++ backend/controllers/inicio.controller.js
@@ -0,0 +1,82 @@
+const { get } = require('mongoose');
+const { insertMany, update } = require('../models/inicio.dto.js');
+const INICIO = require('../models/inicio.dto.js');
+
+const CONTROLADOR = {
+    /**
+     * Metodo para insertar datos del inicio.
+     * @param {*} req 
+     * @param {*} res 
+     */
+    async insert(req, res) {
+        try {
+            let inicio = await INICIO.create(req.body);
+            res.status(201).send({
+                message: 'Datos del inicio insertado correctamente.',
+                inicio
+            });
+        } catch(e) {
+            res.status(500).send({
+                message: 'Error al insertar datos del inicio.',
+                e
+            });
+        }         
+    },
+
+    /**
+     * Metodo para recoger datos del inicio.
+     * @param {*} req 
+     * @param {*} res 
+     */
+    async get(req, res) {
+        try{
+            let inicio = await INICIO.find({});
+            res.status(200).send(inicio);
+        } catch(e) {
+            res.status(500).send({
+                message: 'Error al recoger datos del inicio',
+                e
+            });
+        }
+    },
+
+    /**
+     * Metodo para actualizar datos del inicio.
+     * @param {*} req 
+     * @param {*} res 
+     */
+    async update(req, res) {
+        try {
+            let inicio = await INICIO.findOneAndUpdate(req.body, req.body);
+            res.status(201).send({
+                message: 'Datos del inicio actualizado correctamente.',
+                inicio
+            });
+        } catch(e) {
+            res.status(500).send({
+                message: 'Error al actualizar los datos del inicio.',
+                e
+            });
+        }
+    },
+
+    /**
+     * Metodo para eliminar los datos del inicio.
+     * @param {*} req 
+     * @param {*} res 
+     */
+    async delete(req, res) {
+        try {
+            let inicio = await INICIO.deleteOne(req.body);
+            res.status(201).send({
+                message: 'Datos del inicio eliminados correctamente.',
+                inicio
+            });
+        } catch(e) {
+            res.status(500).send({
+                message: 'Error al eliminar los datos del inicio',
+                e
+            });
+        }
+    },
+}
