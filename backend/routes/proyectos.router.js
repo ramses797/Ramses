42567277c4b772c8aec2b7d212a64d54a2f5016d
Ramses797 <rmorenor97@gmail.com>
2021-01-20 19:00:41 +0100

    PROYECTOS: router -> terminado

---
+++ backend/routes/proyectos.router.js
@@ -0,0 +1,13 @@
+const EXPRESS = require('express');
+
+const PROYECTOS = require('../controllers/proyectos.controller');
+
+const ROUTER = EXPRESS.Router();
+
+ROUTER.post('/insert', PROYECTOS.insert);
+ROUTER.get('/get', PROYECTOS.getAll);
+ROUTER.get('/get/:id', PROYECTOS.getOneById);
+ROUTER.update('/update/:id', PROYECTOS.updateOneById);
+ROUTER.delete('/delete/:id', PROYECTOS.delete)
+
+module.exports = ROUTER;
