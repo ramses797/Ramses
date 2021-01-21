2032494f2ece6acafdcbc84f0a95b8839cb1f89e
Ramses797 <rmorenor97@gmail.com>
2021-01-21 20:10:01 +0100

    cambios en el header y inicio creado

--- frontend/src/app/modules/shared/header/header.component.ts
+++ frontend/src/app/modules/shared/header/header.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  public screen;
   constructor() { }
 
   ngOnInit(): void {
+    this.screen = screen.width;
+    console.log(this.screen);
   }
 
 }
