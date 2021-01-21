b45c410d9af5f47886ae5dc9b00bee1d203a396c
Ramses797 <rmorenor97@gmail.com>
2021-01-21 10:35:17 +0100

    archivos de header y footer creados

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/shared/header/header/header.component';
+import { FooterComponent } from './modules/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
