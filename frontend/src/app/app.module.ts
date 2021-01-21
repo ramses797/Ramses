3ad1fedcecd825120bed6714524bc26fc8f8c5d5
Ramses797 <rmorenor97@gmail.com>
2021-01-21 12:20:24 +0100

    empezando el header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './modules/shared/header/header/header.component';
+import { HeaderComponent } from './modules/shared/header/header.component';
 import { FooterComponent } from './modules/shared/footer/footer.component';
 
 @NgModule({
