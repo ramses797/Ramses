baaacd6f7034ca19c0fceef1bd5ad0f356f5064c
Ramses797 <rmorenor97@gmail.com>
2021-01-29 12:19:35 +0100

    vista sobre mi terminada

--- frontend/src/app/modules/sobre-mi/sobre-mi.component.ts
+++ frontend/src/app/modules/sobre-mi/sobre-mi.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sobre-mi.component.scss']
 })
 export class SobreMiComponent implements OnInit {
+  estudios: string = 'estudios';
+  trabajos: string = 'trabajos';
+  cursos: string = 'cursos';
+  eleccion: string = 'estudios';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cambioVista(vista) {
+    this.eleccion = vista;
+    console.log(this.eleccion);
   }
 
 }
