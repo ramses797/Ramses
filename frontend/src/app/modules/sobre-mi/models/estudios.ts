33fce50ccb76d0d482d8bacfc92eab252511e419
Ramses797 <rmorenor97@gmail.com>
2021-01-29 17:28:49 +0100

    modelos de sobreMi creados

---
+++ frontend/src/app/modules/sobre-mi/models/estudios.ts
@@ -0,0 +1,8 @@
+export type Estudios = {
+    fechaInicio: string,
+    fechaFin: string,
+    localizacion: string,
+    centro: string,
+    titulo: string,
+    aprendizaje: string
+}
---
+++ frontend/src/app/modules/sobre-mi/models/trabajos.ts
@@ -0,0 +1,8 @@
+export type Trabajos = {
+    fechaInicio: string,
+    fechaFin: string,
+    localizacion: string,
+    institucion: string,
+    puesto: string,
+    tareas: string
+}
