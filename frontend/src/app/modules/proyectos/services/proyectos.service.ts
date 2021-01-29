4af6b3db79dd427771f8cb3774ac2c4106670c1e
Ramses797 <rmorenor97@gmail.com>
2021-01-29 17:49:29 +0100

    proyectos.service creado

---
+++ frontend/src/app/modules/proyectos/services/proyectos.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { CrudService } from 'src/app/modules/shared/services/crud.service';
+import { PREFIX } from 'src/app/modules/shared/config/webApiPrefix.config';
+import { Proyectos } from 'src/app/modules/proyectos/models/proyectos';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ProyectosService extends CrudService<Proyectos> {
+
+    constructor(http: HttpClient) {
+        super(http);
+        this.withUrlPrefix(PREFIX.proyectos);
+    }
+}
