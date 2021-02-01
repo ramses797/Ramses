e329d9c8275150d8f826ef098979bfb8471aec23
Ramses797 <rmorenor97@gmail.com>
2021-02-01 16:52:50 +0100

    BACKEND -> contacto acabado correctamente

--- backend/controllers/contacto.controller.js
+++ backend/controllers/contacto.controller.js
@@ -1,19 +1,74 @@
+const { update } = require('../models/contacto.dto');
 const CONTACTO = require('../models/contacto.dto');
-//const NODEMAILER = require('nodemailer');
+const { delete } = require('./inicio.controller');
 
 const CONTROLADOR = {
     async insert(req, res) {
         try {
             let contacto = await CONTACTO.create(req.body);
-            res.status(200).send({
-                message: 'correo enviado',
+            res.status(201).send({
+                message: 'Contacto insertado correctamente.',
                 contacto
             });
         } catch(e) {
             res.status(500).send({
-                message: 'error al enviar el correo',
+                message: 'Error al insertar el contacto.',
                 e
             });
+        }
+    },
+
+    async get(req, res) {
+        try {
+            let contacto = await CONTACTO.find({});
+            res.status(200).send(contacto)
+        } catch(e) {
+            res.status(500).send({
+                message: 'Error al leer los contactos.',
+                e
+            })
+        }
+    },
+
+    async getOneById(req, res) {
+        try {
+            let contacto = await CONTACTO.findOne(req.body._id);
+            res.status(200).send(contacto);
+        } catch(e) {
+            res.status(500).send({
+                message: 'Error al leer un contacto',
+                e
+            });
+        }
+    },
+
+    async update(req,res) {
+        try {
+            let contacto = await CONTACTO.findOneAndUpdate(req.body._id, req.body);
+            res.status(201).send({
+                message: 'Contacto actualizado correctamente.',
+                contacto
+            });
+        } catch(e) {
+            res.status(500).send({
+                message: 'Error al actualizar el contacto.',
+                e
+            })
+        }
+    },
+
+    async delete(req, res) {
+        try {
+            let contacto = await CONTACTO.deleteOne(req.body._id);
+            res.status(201).send({
+                message: 'Contacto eliminado correctamente.',
+                contacto
+            })
+        } catch(e) {
+            res.status(500).send({
+                message: 'Error al eliminar el contacto.',
+                e
+            })
         }
     }
 };
--- backend/routes/contacto.router.js
+++ backend/routes/contacto.router.js
@@ -5,5 +5,9 @@ const CONTACTO = require('../controllers/contacto.controller');
 const ROUTER = EXPRESS.Router();
 
 ROUTER.post('/insert', CONTACTO.insert);
+ROUTER.get('/get', CONTACTO.get);
+ROUTER.get('/getOneById', CONTACTO.getOneById);
+ROUTER.put('/update', CONTACTO.update);
+ROUTER.delete('/delete', CONTACTO.delete);
 
 module.exports = ROUTER;
