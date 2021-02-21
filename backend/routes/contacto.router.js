5391cd70c542ca78db899d27ff58d23deff69820
Ramses797 <rmorenor97@gmail.com>
2021-02-21 18:53:19 +0100

    rutas pasadas sin el _

--- backend/routes/contacto.router.js
+++ backend/routes/contacto.router.js
@@ -7,7 +7,7 @@ const ROUTER = EXPRESS.Router();
 ROUTER.post('/insert', CONTACTO.insert);
 ROUTER.get('/get', CONTACTO.get);
 ROUTER.get('/getOneById', CONTACTO.getOneById);
-ROUTER.put('/update', CONTACTO.update);
-ROUTER.delete('/delete/:_id', CONTACTO.delete);
+ROUTER.put('/update/:id', CONTACTO.update);
+ROUTER.delete('/delete/:id', CONTACTO.delete);
 
 module.exports = ROUTER;
--- backend/routes/cursos.router.js
+++ backend/routes/cursos.router.js
@@ -7,7 +7,7 @@ const ROUTER = EXPRESS.Router();
 ROUTER.post('/insert', CURSOS.insert);
 ROUTER.get('/get', CURSOS.get);
 ROUTER.get('/getOneById', CURSOS.getOneById);
-ROUTER.put('/update', CURSOS.update);
-ROUTER.put('/delete/:_id', CURSOS.delete);
+ROUTER.put('/update/:id', CURSOS.update);
+ROUTER.delete('/delete/:id', CURSOS.delete);
 
 module.exports = ROUTER;
